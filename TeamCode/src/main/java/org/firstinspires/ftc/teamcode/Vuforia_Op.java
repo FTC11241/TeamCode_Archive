4e5352a873440ea2464f9cc32baa6782c2341def
rmorgan <rmorgan@frhsd.com>
2017-08-16 14:50:59 -0400

    Just a test of the VCS making changes in the comments to reference video that I watched

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia_Op.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia_Op.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 /**
  * Created by rmorgan on 8/11/2017 mod. 8/16/2017 practicing JAVA/Vuforia using
  * youtube https://www.youtube.com/watch?v=2z-o9Ts8XoE
- * and GitHub https://www.youtube.com/watch?v=Ldmc757EXaE&feature=youtu.be&t=1m5s
+ * GitHub https://www.youtube.com/watch?v=Ldmc757EXaE&feature=youtu.be&t=1m5s
  */
 
 public class Vuforia_Op extends LinearOpMode {
